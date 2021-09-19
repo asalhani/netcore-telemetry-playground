a3c40963a742f1583a5f565d3e768ebe8c8c732f
Adib Ahmad Salhani <asalhani@elm.sa>
2021-09-19 16:28:44 +0300

    ISP-10445 - working on enahnce logging

---
+++ Common/RefitMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class AuthenticationHeaderHandler: DelegatingHandler
+    {
+        public AuthenticationHeaderHandler()
+        {
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
